ff2b76914fab785b6fc92ea69e7162f1d456aca8
Srinath <srinath.15cs@kct.ac.in>
2018-04-05 15:18:50 +0530

    UI Changes

--- users/static/theme/js/contactform.js
+++ users/static/theme/js/contactform.js
@@ -80,7 +80,7 @@ jQuery(document).ready(function($) {
         else var str = $(this).serialize();		
             $.ajax({
                 type: "POST",
-                url: "contactform/contactform.php",
+                url: "/contact",
                 data: str,
                 success: function(msg){
                    // alert(msg);
